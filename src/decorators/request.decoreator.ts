75daef7ce326a3e044faba7eb58ee46d5249e825
Mahdi Azari <azarifard.mahdi@gmail.com>
2024-02-06 00:58:20 +0330

    Add request decorator

---
+++ src/decorators/request.decoreator.ts
@@ -0,0 +1,16 @@
+export function Map(method: string, path: string, middleware: any) {
+    return function (target: any, propertyKey: string | symbol, descriptor: PropertyDescriptor) {
+        if (!Reflect.hasMetadata("routerPath", target)) {
+            Reflect.defineMetadata("routerPath", [], target);
+        }
+        const routes = Reflect.getMetadata("routerPath", target);
+        routes.push({
+            method,
+            path,
+            propertyKey,
+            middleware
+        })
+        Reflect.defineMetadata("routerPath", routes, target);
+
+    }
+}
