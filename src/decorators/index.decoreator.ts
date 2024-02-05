763bc21eab2f6b60fca6205fd42a35d3f937f773
Mahdi Azari <azarifard.mahdi@gmail.com>
2024-02-06 01:00:29 +0330

    Add indrx Route decorator

---
+++ src/decorators/index.decoreator.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { Request, Response, NextFunction } from "express";
+import { IRoute } from "../interfaces/router.interface";
+class Route implements IRoute {
+    public basePath: string;
+    public router: Router = Router();
+    public ControllerClass: any;
+    public controller: any;
+
+
+    constructor(Controller: any) {
+        this.ControllerClass = Controller;
+        this.controller = new Controller();
+        this.basePath = "";
+        this.setupRoutes();
+    }
+
+    private setupRoutes() {
+        this.basePath = Reflect.getMetadata("base", this.ControllerClass);
+
+        const routes = Reflect.getMetadata("routerPath", this.controller);
+
+        routes.forEach((route: any) => {
+            (this.router as any)[route.method](
+                route.path,
+                route.middleware,
+                (req: Request, res: Response, next: NextFunction) => {
+                    this.controller[route.propertyKey](req, res, next);
+                })
+        });
+
+    }
+}
+
+export default Route;
