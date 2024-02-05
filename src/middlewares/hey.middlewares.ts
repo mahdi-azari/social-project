edd792ceaa85b410aa4587d14460dacc26b84d13
Mahdi Azari <azarifard.mahdi@gmail.com>
2024-02-06 01:03:06 +0330

    Add middlewares

---
+++ src/middlewares/hey.middlewares.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+
+export function hey(req : Request , res: Response , next : NextFunction) {
+    try {
+        console.log("middleware");
+        next();
+    } catch (error) {
+        console.log(error);
+    }
+}
