30cb783ca73939c810460fb2109702fb4d064837
Mahdi Azari <azarifard.mahdi@gmail.com>
2024-02-06 01:14:21 +0330

    Add user service

---
+++ src/services/user.service.ts
@@ -0,0 +1,16 @@
+import { IUser } from './../interfaces/user.interface';
+import userModel, { UserDocument } from "../models/user.model";
+import { Model } from "mongoose";
+
+
+class UserService {
+    private readonly UserModel: Model<UserDocument> = userModel;
+
+    public async createUser(userData: any): Promise<IUser> {
+        return await this.UserModel.create(userData);
+    }
+
+
+}
+
+export default UserService;
